edccd91a3cfe4b1069e3de28466d7cc585e4ffd7
Michal Repec <mikerepec@gmail.com>
2019-02-16 12:59:59 +0100

    Implement methods provider from yaml file + setup get/set serializer

--- src/Services/MethodsFromYmlProvider.php
+++ src/Services/MethodsFromYmlProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\DTO\AdditionalDataFieldDTO;
 use App\DTO\MethodDTO;
 
 final class MethodsFromYmlProvider implements MethodsProvider
@@ -11,14 +12,52 @@ final class MethodsFromYmlProvider implements MethodsProvider
      */
     private $methods;
 
-    public function __construct()
+    /**
+     * @param array $available_methods
+     */
+    public function __construct($available_methods)
     {
-        // TODO:: Read methods from yml file here
-        $this->methods = [];
+        $this->methods = array_map([$this, 'makeDTO'], $available_methods);
     }
 
+    /**
+     * @return MethodDTO[]
+     */
     public function getMethods(): array
     {
         return $this->methods;
+    }
+
+    /**
+     * @param array $method
+     *
+     * @return MethodDTO
+     */
+    private function makeDTO($method): MethodDTO
+    {
+        /** @var AdditionalDataFieldDTO[] $additionalData */
+        $additionalData = array_map([$this, 'makeAdditionalDataDTO'], $method['additionalData']);
+
+        return new MethodDTO(
+            $method['name'],
+            $method['label'],
+            $method['types'],
+            $additionalData
+        );
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return AdditionalDataFieldDTO
+     */
+    private function makeAdditionalDataDTO($data): AdditionalDataFieldDTO
+    {
+        return new AdditionalDataFieldDTO(
+            $data['name'],
+            $data['label'],
+            $data['type'],
+            $data['required']
+        );
     }
 }
